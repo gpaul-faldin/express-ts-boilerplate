3071bff8144018f91fe8310e1379728dd641d663
gpaul <40648068+gpaul-faldin@users.noreply.github.com>
2024-11-20 09:38:04 +0100

    test: adding badly formatted file

--- src/test-lint.ts
+++ src/test-lint.ts
@@ -1,3 +1,4 @@
+// This file has intentionally bad formatting and lint issues
 const badlyFormattedFunction = async () => {
   const unused = 'this will trigger a lint warning';
   return await Promise.resolve('this is bad practice to await unnecessary promises');
